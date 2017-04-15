d7bce75751c60bf643786e4383981b3620ce2154
Auriles El Haddad <auriles0404@gmail.com>
2017-04-15 14:37:04 +0200

    création des components

---
+++ chatBox/src/components/app.js
@@ -0,0 +1,21 @@
+// React
+import React from 'react';
+import Formulaire from './formulaire';
+import Message from './message';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="box">
+        <div>
+          <div className="messages">
+            <Message />
+          </div>
+          <Formulaire />
+        </div>
+      </div>
+      );
+  }
+}
+
+export default App;
