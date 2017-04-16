79822cdfa8f42c7a508cb5f87f84e40ca9703864
Auriles El Haddad <auriles0404@gmail.com>
2017-04-16 15:55:32 +0200

    synchroniser avec firebase

---
+++ chatBox/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyBnyB8DQiMocfeECbizR8qEvO68mI644RY",
+  authDomain: "my-chatbox-ca42c.firebaseapp.com",
+  databaseURL: "https://my-chatbox-ca42c.firebaseio.com"
+});
+
+export default base;
